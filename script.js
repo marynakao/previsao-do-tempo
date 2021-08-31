6b8b49fef5b6c41cc21594b24a2e7c7d0572d898
Maryna Kao <86077339+marynakao@users.noreply.github.com>
2021-08-31 11:03:54 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,19 @@
-// Aprendendo a fazer requisição interna. Possibilita a comunicação com diversos serviços.
-// async = funcão assíncrona
-
 document.querySelector('.busca').addEventListener('submit', async (event) => {
     event.preventDefault();
 
     let input = document.querySelector('#searchInput').value;
-
-    // algo foi digitado
+    
     if (input !== ' ') {
-    clearInfo(); // limpa a tela para exibir somente o carregando após outra pesquisa
+    clearInfo();
 
         showWarning('Carregando...');
-        
-        // encodeURI transforma o input em código de pesquisa url
+        // Requisição externa da API
         let url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(input)}&appid=df0dbedef4676649bc0a5d6bd5084618&units=metric&lang=pt_br`;
 
         let results = await fetch(url);
 
-        // peguei a url e vou transformar o resultado em objeto JS
         let json = await results.json();
-
+        // Propriedades de previsão do tempo que serão utilizadas
         if (json.cod === 200) {
             showInfo({
                 name: json.name,
@@ -28,9 +22,6 @@ document.querySelector('.busca').addEventListener('submit', async (event) => {
                 tempIcon: json.weather[0].icon,
                 windSpeed: json.wind.speed,
                 windAngle: json.wind.deg
-                /*feelLike: json.main.feels_like,
-                tempMax: json.main.temp_max,
-                tempMin: json.main.temp_min*/
             });
         }
         else {
@@ -38,7 +29,7 @@ document.querySelector('.busca').addEventListener('submit', async (event) => {
         }
     } 
 });
-// exibe as infos de tempo
+//Exibe as infos das propriedades selecionadas
 function showInfo(json) {
     showWarning('');
 
@@ -46,15 +37,15 @@ function showInfo(json) {
     document.querySelector('.tempInfo').innerHTML = `${json.temp}<sup>ºC</sup>`;
     document.querySelector('.ventoInfo').innerHTML = `${json.windSpeed} <span>km/h</span>`;
 
-    // altera o icon
+    // Altera o icon
     document.querySelector('.temp img').setAttribute('src', `http://openweathermap.org/img/wn/${json.tempIcon}@2x.png`);
     
-    //altera o angulo da rotação do vento
+    // Altera o angulo da rotação do vento
     document.querySelector('.ventoPonto').style.transform = `rotate(${json.windAngle - 90}deg)`;
 
     document.querySelector('.resultado').style.display = 'block';
 }
-// limpa a tela para exibir somente o aviso de não encontrado
+// Limpa a tela para exibir somente o aviso de não encontrado
 function clearInfo() {
     showWarning('');
     document.querySelector('.resultado').style.display = 'none';
